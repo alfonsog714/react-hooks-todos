592ed7f937919ccc37bfd584fbd9820fbe9e4e8c
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-24 09:12:48 -0700

    Installed material-ui icons

--- src/Todo.js
+++ src/Todo.js
@@ -2,12 +2,20 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/core/Delete";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
 function Todo({ task, completed }) {
   return (
     <ListItem>
-      <Checkbox checked={completed} />
+      <Checkbox tabIndex={-1} checked={completed} />
       <ListItemText>{task}</ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   );
 }
