db05e092e479baa66a287ad0f6a437be7da503db
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-25 09:30:16 -0700

    Fixed delete icon

--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/core/Delete";
+import DeleteIcon from "@material-ui/icons/Delete";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
 function Todo({ task, completed }) {
